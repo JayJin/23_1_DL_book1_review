20b6e48a97ca627b3a4746e7ff1b456745ca1808
Jay <jvidorca@gmail.com>
2023-03-09 13:14:23 +0900

    update chap02 files(md, src codes, ...)

---
+++ ch02/ch02_src_code_04.py
@@ -0,0 +1,20 @@
+from ch02_src_code_03 import AND, NAND, OR
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+
+print(XOR(0, 0))
+print(XOR(1, 0))
+print(XOR(0, 1))
+print(XOR(1, 1))
+
+# 출력 결과
+'''
+0
+1
+1
+0
+'''
